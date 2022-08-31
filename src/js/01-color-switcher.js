bcab4261038f46ecdf4456e4e6708e6fe7018b91
OlenaDoroshenko <70623973+OlenaDoroshenko@users.noreply.github.com>
2022-08-31 08:25:13 +0300

    Colorizer added

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const startButton = document.querySelector("button[data-start]");
+const stopButton = document.querySelector("button[data-stop]");
+const body = document.querySelector("body");
 
+let timerId = null;
+stopButton.disabled = true;
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+startButton.addEventListener("click", startColorizer);
+stopButton.addEventListener("click", stopColorizer);
+
+function startColorizer(){
+    timerId = setInterval(() => {
+        console.log("generate color");
+        body.style.background = getRandomHexColor();
+    }, 1000);
+    startButton.disabled = true;
+    stopButton.disabled = false;
+}
+
+function stopColorizer(){
+    clearInterval(timerId);
+    startButton.disabled = false;
+    stopButton.disabled = true;
+}
