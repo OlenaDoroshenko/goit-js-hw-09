b3f9e72b049aa7b2c0099bd6e2404f7b7519acad
OlenaDoroshenko <70623973+OlenaDoroshenko@users.noreply.github.com>
2022-09-03 16:26:23 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,47 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', submitForm);
+
+let timerId = null;
+
+function submitForm(e) {
+  e.preventDefault();
+  let delay = Number(form.elements.delay.value);
+
+  for (
+    let position = 1;
+    position <= form.elements.amount.value;
+    position += 1
+  ) {
+    let promise = createPromise(position, delay);
+
+    promise
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+
+      delay += Number(form.elements.step.value);
   }
+  clearInterval(timerId);
+}
+
+function createPromise(position, delay) {
+
+  const shouldResolve = Math.random() > 0.3;
+
+  return new Promise((resolved, rejected) => {
+
+    timerId = setTimeout(() => {
+      if (shouldResolve) {
+        resolved({ position, delay });
+      } else {
+      rejected({ position, delay });
+      }      
+    }, delay); 
+  });
 }
